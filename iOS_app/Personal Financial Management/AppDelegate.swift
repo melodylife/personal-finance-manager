6ff0157c3c06f80894039d53ec360a2d8b1f75fe
chsun <chsun@chsun-macbookpro.roam.corp.google.com>
2016-07-02 21:36:59 +0800

    Add Firebase to the Project

--- iOS_app/Personal Financial Management/AppDelegate.swift
+++ iOS_app/Personal Financial Management/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -24,6 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             print("The service is off")
         }
         
+        FIRApp.configure()
         return true
     }
 
